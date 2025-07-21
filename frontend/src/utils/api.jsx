abfddaff2ee8ecd6717b2248da9f4d37d074cb4a
Nikhil Jagtap <jagtapnikhil1806@gmail.com>
2025-07-21 12:29:45 +0530

    Minor Updates

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // Replace with your backend URL
+const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL; // Replace with your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
